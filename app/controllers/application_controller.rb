8d7d7f446502c32eaf53bdf6c992fa5978cdf2bb
thomastrzos <trzos.tomasz@gmail.com>
2018-12-31 14:05:37 +0100

    Add send_email_with_order endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,18 @@ class ApplicationController < ActionController::Base
     render plain: "eshop-email-sender, env: #{Rails.env}"
   end
 end
+
+
+def send_email_with_order
+  if false # FIXME: params[:token] != PUBSUB_VERIFICATION_TOKEN
+    render json: { error: 'Unauthorized' }, status: :unprocessable_entity
+  else
+    # slug = Base64.decode64(params[:message][:data])
+
+    # if EmailSenderService.new.subscribe_contact(slug)
+    #   render json: {}, status: :ok
+    # else
+    #   render json: { error: 'Subscription failed' }, status: :unprocessable_entity
+    # end
+  end
+end
