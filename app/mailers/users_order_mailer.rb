6cc217fd37c6dbcb5561a017e0ed3d2769c846e1
thomastrzos <trzos.tomasz@gmail.com>
2019-01-01 23:16:25 +0100

    Return params instead of slug in UsersOrderMailer

--- app/mailers/users_order_mailer.rb
+++ app/mailers/users_order_mailer.rb
@@ -1,6 +1,7 @@
 class UsersOrderMailer < ApplicationMailer
   def send_order_summary(email, params)
-    @slug = Base64.decode64(params[:message][:attributes][:slug])
+    # @slug = Base64.decode64(params[:message][:attributes][:slug])
+    @params = params.to_s
     mail(to: email, subject: 'Order summary from trzos-tomasz.pl')
   end
 end
