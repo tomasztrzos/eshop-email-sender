45bb2702ab28520c3eca532d57e4bcb92d1b98bb
thomastrzos <trzos.tomasz@gmail.com>
2019-01-02 21:03:21 +0100

    Add params to UsersOrderMailer, update send_order_summary view

--- app/mailers/users_order_mailer.rb
+++ app/mailers/users_order_mailer.rb
@@ -1,7 +1,11 @@
 class UsersOrderMailer < ApplicationMailer
   def send_order_summary(email, params)
-    # @slug = Base64.decode64(params[:message][:attributes][:slug])
-    @params = params.to_s
+    params = pubsub_msg['message']['attributes']
+    @orders_date = params['created_at']
+    @products = params['products']
+    @total_price = params['total_price']
+    @shipping_details = params['orders_shipping_details']
+
     mail(to: email, subject: 'Order summary from trzos-tomasz.pl')
   end
 end
