e3038e8558875f37de6c8589f4760da36cb39fa0
thomastrzos <trzos.tomasz@gmail.com>
2019-01-02 23:15:08 +0100

    Parsing in UsersOrderMailer

--- app/mailers/users_order_mailer.rb
+++ app/mailers/users_order_mailer.rb
@@ -1,10 +1,10 @@
 class UsersOrderMailer < ApplicationMailer
   def send_order_summary(email, params)
-    pubsub_msg = params['message']['attributes']
+    pubsub_msg = JSON.parse(params['message']['attributes'])
     @orders_date = pubsub_msg['created_at']
-    @products_array = JSON.parse(pubsub_msg['products'])
+    @products_array = pubsub_msg['products']
     @total_price = pubsub_msg['total_price']
-    @shipping_details = JSON.parse(pubsub_msg['orders_shipping_details'])
+    @shipping_details = pubsub_msg['orders_shipping_details']
 
     mail(to: email, subject: 'Order summary from trzos-tomasz.pl')
   end
