c8af782daeb8493ef1347e4eb3bb5019bd2e3440
thomastrzos <trzos.tomasz@gmail.com>
2019-01-06 21:32:50 +0100

    Tmp commit - show params in email

--- app/mailers/users_order_mailer.rb
+++ app/mailers/users_order_mailer.rb
@@ -1,10 +1,14 @@
 class UsersOrderMailer < ApplicationMailer
   def send_order_summary(email, params)
-    pubsub_msg = JSON.parse(params['message']['attributes'])
-    @orders_date = pubsub_msg['created_at']
-    @products_array = pubsub_msg['products']
-    @total_price = pubsub_msg['total_price']
-    @shipping_details = pubsub_msg['orders_shipping_details']
+    @pubsub_msg = params['message']['attributes']
+    # @orders_date = pubsub_msg['created_at']
+    # @products_array = pubsub_msg['products']
+    # @total_price = pubsub_msg['total_price']
+    # @shipping_details = pubsub_msg['orders_shipping_details']
+    @orders_date = ''
+    @products_array = []
+    @total_price = ''
+    @shipping_details = ''
 
     mail(to: email, subject: 'Order summary from trzos-tomasz.pl')
   end
